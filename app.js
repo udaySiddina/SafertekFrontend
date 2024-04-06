4f12d51ed70c271f07506cc64ce17a380ad456e3
udaySiddina <148368454+udaySiddina@users.noreply.github.com>
2024-04-06 11:47:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// tasks ---
+// should calculate total
+// calculate percentage
+// calculate grade
+// calculate pass/fail
+// show output
+
+
+const calcy = () =>{
+    let wd = document.getElementById('wd').value;
+    let maths = document.getElementById('maths').value;
+    let comp = document.getElementById('comp').value;
+    let phy = document.getElementById('phy').value;
+    let grades = "";
+  
+  
+    let totalGrades = (parseFloat(wd) + parseFloat(maths) + parseFloat(comp) + parseFloat(phy)).toFixed(2);
+  
+    let perc =(((totalGrades/400) * 100).toFixed(2));
+    
+  
+    debugger;
+  
+    if(perc <= 100  && perc >= 80){
+      grades = 'A';
+    }else if(perc <= 79  && perc >= 60){
+       grades = 'B';
+    }else if(perc <= 59  && perc >= 40){
+       grades = 'C';
+    }else{
+       grades = 'F';
+    }
+  
+    if(perc >= 39.5){
+      document.getElementById('showData').innerHTML = ` Out of 400 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}.  `
+      document.getElementById('result').innerHTML = ` PASS `
+    }
+    else{
+      document.getElementById('showData').innerHTML = ` Out of 400 your total is  ${totalGrades} and percentage is ${perc}%. <br> Your grade is ${grades}. `
+      document.getElementById('result').innerHTML = ` FAIL `
+    }
+  
+    
+  }
